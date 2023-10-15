cda0440b681007c074004aed63816b55b0304786
Andrey.Skripay <lordktan@gmail.com>
2023-10-15 13:25:09 +0500

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
+import java.time.LocalDate;
 public class Main {
-    public static void main(String[] args) {
+    public static void suggestAppVersion(int osType, int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
 
-        // Задание 1
-        int year = 2024; // год для проверки
-        checkLeapYear(year);
+        if (deviceYear > currentYear) {
+            System.out.println("Устройство из будущего. Проверьте год выпуска.");
+            return;
+        }
+
+        if (deviceYear < 2015) {
+            if (osType == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if (osType == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Некорректный тип операционной системы");
+            }
+        } else {
+            if (osType == 0) {
+                System.out.println("Установите обычную версию приложения для iOS");
+            } else if (osType == 1) {
+                System.out.println("Установите обычную версию приложения для Android");
+            } else {
+                System.out.println("Некорректный тип операционной системы");
+            }
+        }
     }
 
-    public static void checkLeapYear(int year) {
-        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-            System.out.println(year + " год — високосный год");
-        } else {
-            System.out.println(year + " год — невисокосный год");
-        }
+    public static void main(String[] args) {
+        int osType = 0; // 0 - iOS, 1 - Android
+        int clientDeviceYear = 2015; // год выпуска устройства
+
+        suggestAppVersion(osType, clientDeviceYear);
     }
 }
