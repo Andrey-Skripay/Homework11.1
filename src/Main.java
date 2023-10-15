c19035ed1191ae84b147ca454f3fa485bd44d05a
Andrey.Skripay <lordktan@gmail.com>
2023-10-15 13:17:03 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        // Задание 1
+        int year = 2024; // год для проверки
+        checkLeapYear(year);
+    }
+
+    public static void checkLeapYear(int year) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
+        }
     }
 }
