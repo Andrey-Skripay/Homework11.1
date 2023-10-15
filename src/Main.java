f1a816ab4d11badc9cd66da6834d157374749d7b
Andrey.Skripay <lordktan@gmail.com>
2023-10-15 13:32:53 +0500

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,21 @@
-import java.time.LocalDate;
 public class Main {
-    public static void suggestAppVersion(int osType, int deviceYear) {
-        int currentYear = LocalDate.now().getYear();
-
-        if (deviceYear > currentYear) {
-            System.out.println("Устройство из будущего. Проверьте год выпуска.");
-            return;
-        }
-
-        if (deviceYear < 2015) {
-            if (osType == 0) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else if (osType == 1) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Некорректный тип операционной системы");
-            }
-        } else {
-            if (osType == 0) {
-                System.out.println("Установите обычную версию приложения для iOS");
-            } else if (osType == 1) {
-                System.out.println("Установите обычную версию приложения для Android");
-            } else {
-                System.out.println("Некорректный тип операционной системы");
-            }
-        }
+    public static void main(String[] args) {
+        int deliveryDistance = 95;
+        int deliveryDays = calculateDeliveryDays(deliveryDistance);
+        System.out.println("Потребуется дней: " + deliveryDays);
     }
 
-    public static void main(String[] args) {
-        int osType = 0; // 0 - iOS, 1 - Android
-        int clientDeviceYear = 2015; // год выпуска устройства
-
-        suggestAppVersion(osType, clientDeviceYear);
+    public static int calculateDeliveryDays(int distance) {
+        if (distance <= 20) {
+            return 1;
+        } else if (distance <= 60) {
+            return 2;
+        } else if (distance <= 100) {
+            return 3;
+        } else {
+            return -1; // Доставка невозможна для расстояния более 100 км
+        }
     }
 }
+
+
